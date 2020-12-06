2886ef8af4ba7f88df333247cf20dd9dd9331178
mddarr <mddarr@gmail.com>
2020-12-06 01:12:54 -0800

    lambda is getting triggered correctly.

--- presigned_url_sqs_event_stack/handlers/verify_upload/app.py
+++ presigned_url_sqs_event_stack/handlers/verify_upload/app.py
@@ -3,20 +3,21 @@ import os
 import json
 
 
-dynamo_endpoint = os.getenv('dynamo_endpoint')
-if dynamo_endpoint == 'cloud':
-    dynamo_resource = boto3.resource('dynamodb')
-else:
-    dynamo_resource = boto3.resource('dynamodb', endpoint_url=dynamo_endpoint)
-
+# dynamo_endpoint = os.getenv('dynamo_endpoint')
+# if dynamo_endpoint == 'cloud':
+#     dynamo_resource = boto3.resource('dynamodb')
+# else:
+#     dynamo_resource = boto3.resource('dynamodb', endpoint_url=dynamo_endpoint)
+#
 TABLE_NAME = os.getenv('user_upload_table')
-table = dynamo_resource.Table(TABLE_NAME)
+
+# table = dynamo_resource.Table(TABLE_NAME)
 
 
 def lambda_handler(event, context):
     # response = {"statusCode": 200, "body": json.dumps({
     #     "presigned": event
     # }), 'headers': {"Access-Control-Allow-Origin": "*"}}
-
+    print(TABLE_NAME)
     print(event)
 
