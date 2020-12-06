1b6549ec9f0b4390f270e1965576d21ebaa2d8e0
mddarr <mddarr@gmail.com>
2020-12-06 02:20:47 -0800

    create an entry in the dynamo table

--- presigned_url_sqs_event_stack/handlers/verify_upload/app.py
+++ presigned_url_sqs_event_stack/handlers/verify_upload/app.py
@@ -2,22 +2,49 @@ import boto3
 import os
 import json
 
+dynamo_endpoint = os.getenv('dynamo_endpoint')
+if dynamo_endpoint == 'cloud':
+    dynamo_resource = boto3.resource('dynamodb')
+else:
+    dynamo_resource = boto3.resource('dynamodb', endpoint_url=dynamo_endpoint)
 
-# dynamo_endpoint = os.getenv('dynamo_endpoint')
-# if dynamo_endpoint == 'cloud':
-#     dynamo_resource = boto3.resource('dynamodb')
-# else:
-#     dynamo_resource = boto3.resource('dynamodb', endpoint_url=dynamo_endpoint)
-#
 TABLE_NAME = os.getenv('user_upload_table')
 
-# table = dynamo_resource.Table(TABLE_NAME)
+table = dynamo_resource.Table(TABLE_NAME)
+region = 'us-west-2'
+
+
+def insert_user_upload(upload):
+    return table.put_item(
+        Item={
+            'user': upload['user'],
+            'filename': upload['filename'],
+            'fileurl': upload['fileurl'],
+        }
+    )
 
 
 def lambda_handler(event, context):
-    # response = {"statusCode": 200, "body": json.dumps({
-    #     "presigned": event
-    # }), 'headers': {"Access-Control-Allow-Origin": "*"}}
-    print(TABLE_NAME)
-    print(event)
+    """
+    This lambda function is triggered by an S3 object creation notification.  This function extracts the fileurl, filename
+    & determines the userid from the event & creates an entry in the dynamo table.
 
+    :param event:
+    :param context:
+    :return:
+    """
+    records = event['Records']
+    r1 = records[0]
+    s3_record = r1['s3']
+    bucket = s3_record['bucket']['name']
+    key = s3_record['object']['key']
+
+    username = key.split('/')[0]
+    filename = key.split('/')[-1]
+    object_url = 'http://{}-{}.amazonaws.com/{}'.format(bucket, region, key)
+
+    print("FILENAME " + filename)
+    print("KEY" + key  )
+
+    upload = {'filename': filename, 'fileurl': object_url, 'user': username}
+    insert_user_upload(upload)
